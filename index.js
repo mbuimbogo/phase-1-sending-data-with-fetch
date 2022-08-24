2a879fcab963affd115af151ea45ed227d51bd02
“mbuimbogo” <peter.mbogo@student.moringaschool.com>
2022-08-24 22:39:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+function submitData(name, email){
+   return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        name:name, 
+        email:email,
+        })
+   })
+   .then(res => res.json())
+   .then(user => {
+    let id = user.id;
+    let p = document.createElement('p')
+    p.textContent = id;
+    document.body.appendChild(p)
+   })
+.catch(function(error){
+    let er = document.createElement("p");
+    er.textContent = error.message;
+    document.body.appendChild(er);
+});
+
+}
+submitData('Steve', "steve@steve.com")
